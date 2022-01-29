560292ac9ccb69c643e6a2cfe945eff22ba4ee62
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-29 06:11:03 +0300

    Create configuration classes

--- app/config.py
+++ app/config.py
@@ -1,1 +1,31 @@
 # Contains our app configurations
+
+
+class Config :
+  '''
+  General configuration parent class
+  '''
+  pass
+
+
+
+class ProdConfig(Config) :
+  '''
+  Production configuration child class
+  
+  Args:
+    Config: The parent configuration class with General configuration settings
+  '''
+  pass
+
+
+
+class DevConfig(Config) :
+  '''
+  Development configuration child class
+
+  Args:
+    Config: The parent configuration class with General configuration settings
+  '''
+
+  DEBUG = True
