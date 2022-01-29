f5ac2641f019912b600165a1382983fd676e7891
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-29 16:02:00 +0300

    Use app.errorhandler() function to pass the error received

--- app/error.py
+++ app/error.py
@@ -1,1 +1,12 @@
 # Contains handlers for error pages
+
+from flask import render_template
+from app import app
+
+
+@app.errorhandler(404)
+def fourOwFour(error) :
+  '''
+  Function to render the 404 error page
+  '''
+  return render_template('fourOwFour.html'), 404
