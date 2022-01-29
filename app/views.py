a69310d83b83e9a1fe08a75fb2f6c9e0b501da27
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-29 07:00:09 +0300

    Generate the API

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 # Contains all our view functions
 
+
 from flask import render_template
 from app import app
 
@@ -11,4 +12,6 @@ def index() :
   View root page function that returns the index page and its data
   '''
 
-  return render_template('index.html')
+  title = 'Rejelea - Where updates find you.'
+
+  return render_template('index.html', title = title)
