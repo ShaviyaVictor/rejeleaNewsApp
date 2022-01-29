c3ac7a94edfd3bb8535f263ef225e93b6c70a648
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-29 06:14:48 +0300

    Setting up the configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 # Where the app is initialized
 
 from flask import Flask
+from .config import DevConfig
 
 # Initializing the application
 app = Flask(__name__)
+
+
+# Setting up the app configuration
+app.config.from_object(DevConfig)
+
 
 from app import views
