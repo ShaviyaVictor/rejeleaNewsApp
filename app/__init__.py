cf892ca787011771cc171401dbc2fcc1c3863667
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-29 12:38:51 +0300

    Install bootstrap dependancy

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # Where the app is initialized
 
+
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 # Initializing the application
@@ -10,6 +12,10 @@ app = Flask(__name__, instance_relative_config = True)
 # Setting up the app configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 
 from app import views
