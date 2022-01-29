504e6d29964afff18297b77b2c0b86af6a903516
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-29 17:05:51 +0300

    Instantiating the manager claaand use the add_command method to create a new command server that will launch our application

--- manage.py
+++ manage.py
@@ -6,5 +6,10 @@ from flask_script import Manager, Server
 
 
 
+# Creating app instance
+app = createApp('development')
+manager = Manager(app)
+manager.add_command('server', Server)
+
 if __name__ == '__main__' :
   main.run()
